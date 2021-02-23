911d952f9d9f367a5a22d2acf2ed943889b1363b
hagleyson <hagleyson@gmail.com>
2021-02-23 20:47:50 -0300

    terminando a configuraçao do next

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+export function ExperienceBar() {
+  return (
+    <header className="experienceBar">
+      <span> 0</span>
+
+      <div>
+        <div style={{ width: "50%" }}></div>
+        <span className="current-experience" style={{ left: "50%" }}>
+          380xp
+        </span>
+      </div>
+      <span>680 xp</span>
+    </header>
+  );
+}
